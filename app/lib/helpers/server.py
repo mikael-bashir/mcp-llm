c9c16c68ba973b66a060578fcf252d9d9ed40f7c
mikaelbashir <bashirm-y16@mgs.org>
2025-07-08 17:38:18 +0100

    mcp functionality

---
+++ app/lib/helpers/server.py
@@ -0,0 +1,20 @@
+hard_coded_urls = {
+    "http://localhost:3000/api/example-urls/url1",
+    "http://localhost:3000/api/example-urls/url2",
+    "http://localhost:3000/api/example-urls/url3",
+    "http://localhost:3000/api/example-urls/url4",
+    "http://localhost:3000/api/example-urls/url5",
+    "http://localhost:3000/api/example-urls/url6",
+    "http://localhost:3000/api/example-urls/url7",
+    "http://localhost:3000/api/example-urls/url8",
+    "http://localhost:3000/api/example-urls/url9",
+    "http://localhost:3000/api/example-urls/url10",
+    "http://localhost:3000/api/example-urls/url11",
+    "http://localhost:3000/api/example-urls/url12",
+    "http://localhost:3000/api/example-urls/url13",
+    "http://localhost:3000/api/example-urls/url14",
+    "http://localhost:3000/api/example-urls/url15"
+}
+
+def get_urls():
+    return hard_coded_urls
